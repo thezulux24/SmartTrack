eaa121ef3b9763ea9038641997b74dc2ed441b9f
thezulux24 <thezulux24gmail.com>
2025-09-10 21:57:01 -0500

    Auto-generated commit

---
+++ chunk-2JAYVQRO.js
@@ -0,0 +1,1 @@
+import"./chunk-FK42CRUA.js";var o=[{path:"",loadComponent:()=>import("./chunk-44XAH352.js")},{path:"**",redirectTo:""}];export{o as default};
--- chunk-WCXXVW43.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-FK42CRUA.js";var o=[{path:"",loadComponent:()=>import("./chunk-YJDWG2UZ.js")},{path:"**",redirectTo:""}];export{o as default};
