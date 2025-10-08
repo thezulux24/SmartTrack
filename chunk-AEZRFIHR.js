e07a41c3c291258d26d99b83ceac3c0cce957455
thezulux24 <thezulux24gmail.com>
2025-10-08 18:31:45 -0500

    Auto-generated commit

---
+++ chunk-AEZRFIHR.js
@@ -0,0 +1,1 @@
+import"./chunk-FK42CRUA.js";var t=[{path:"",loadComponent:()=>import("./chunk-PL7S3DNO.js").then(o=>o.TrazabilidadListComponent)}];export{t as TRAZABILIDAD_ROUTES};
--- chunk-HIUZKEEN.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-FK42CRUA.js";var t=[{path:"",loadComponent:()=>import("./chunk-CYYWJVWH.js").then(o=>o.TrazabilidadListComponent)}];export{t as TRAZABILIDAD_ROUTES};
